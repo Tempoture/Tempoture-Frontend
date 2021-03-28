def060be614b047a81fbd88cb52644dc55be4229
Jimmers2001 <jdbatista2001@gmail.com>
2021-03-27 21:56:36 -0400

    Added github/rcos buttons
    Starting the Finishing touches of the info page

--- src/components/Pages/CurrentPages/InfoPage.js
+++ src/components/Pages/CurrentPages/InfoPage.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import * as IoIcons from 'react-icons/io';
 import image from '../../Navbar/Tempoture Logo.jpg'
-
+import gitLogo from '../Images/GithubDesktopLogo.png'
+import RcosR from '../Images/RCOSlogo.png'
 const InfoPage = () => {
     return (
         <content>
@@ -13,16 +13,19 @@ const InfoPage = () => {
                     <p className="body">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
                 <p className="subtitle"> Your Data</p>
                 <p className="body">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
-            <p className = "infoImg">
-                <IoIcons.IoLogoGithub/>
-                <img src={image} width="75rem" />
-            </p>
-            <p className="footer">An RCOS Project :)</p>
-
-        </div>
+                <a href="https://github.com/Tempoture">
+                    <img src={gitLogo} 
+                    width="67px" height="67px"/>
+                </a>
+                <img src={image} width="75px" height="65px"/>
+                <a href="https://rcos.io/">
+                    <img src={RcosR} 
+                    width="61px" height="61px"/>
+                </a>
+                <p className="footer">An RCOS Project :)</p>
+            </div>
        </content>
         );
-
 };
 
 export default InfoPage;
