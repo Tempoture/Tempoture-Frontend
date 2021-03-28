cd2cc30626e3dc3a53b3cfc90608191df7c8a368
crowlc3 <crowlc3@rpi.edu>
2021-03-28 18:45:23 -0400

    LoginPage-Full

--- src/components/Pages/CurrentPages/LogInPage.js
+++ src/components/Pages/CurrentPages/LogInPage.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Button } from 'react-bootstrap';
+import Logo from "./Tempo-Logo-Grey.png"
+import Text_ from "./Tempoture-Text.png"
+import { Text, StyleSheet } from 'react-native';
 
 require('dotenv').config()
 
@@ -25,13 +28,38 @@ const handleLogin = () => {
 
 const LogInPage = (props) => {
   localStorage.setItem('isAuthenticated', false);
-
   return (
     <div className="login">
-      <h1 className="main-heading"> Tempoture </h1>
-      <Button variant="info" type="submit" onClick={handleLogin}>
-        Login with spotify {process.env.SPOTIFY_CLIENT_ID}
-      </Button>
+      <div class="container">
+        <div className = "logo">
+            <img src={Logo} width="150px"></img>
+        </div>
+        <div className = "text">
+            <img src={Text_} width="250px"></img>
+        </div>
+        <div className = "RCOS">
+          An <a className = "Test" href = "google.com">RCOS</a> Project
+        </div>
+        <div className = "Version">
+            Version 1.0 Coming Summer 2021
+        </div>
+        <a className = "What" href = "google.com">
+            What is Tempoture?
+        </a>
+        <div className = "Agree">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+        </div>
+        <div className="Checkbox">
+          <input type="checkbox" className="Check"/>
+          <label className="Label"> Accept User Agreement</label>
+        </div>
+        <Button variant="info" type="submit" onClick={handleLogin}>
+          Login to spotify {process.env.SPOTIFY_CLIENT_ID}
+        </Button>
+      </div>
+      <div className = "Spotify">
+        Don't have Spotify? <a className = "Click" href = "google.com">Click here</a>
+      </div>
     </div>
   );
 };
