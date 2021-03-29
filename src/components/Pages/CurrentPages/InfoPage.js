2eed1ef17872ad0de0a44ef0400ee78e9a627d9e
Jimmers2001 <jdbatista2001@gmail.com>
2021-03-28 20:26:19 -0400

    Incorporated blackbox container for InfoPage
    Set up consistent background container for the page, but the text might be adjusted to have more a gap around the container.

--- src/components/Pages/CurrentPages/InfoPage.js
+++ src/components/Pages/CurrentPages/InfoPage.js
@@ -6,23 +6,30 @@ const InfoPage = () => {
     return (
         <content>
             <div class = "content">
-                <p className="banner">Tempoture</p>
-                <p className="subtitle">Information</p>
+                <div class = "newcontainer">
+                    
+                    <p className="banner">Tempoture</p>
+                    <p className="subtitle">Information</p>
+                        <p className="body">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+                    <p className="subtitle"> Thank you</p>
+                        <p className="body">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+                    <p className="subtitle"> Your Data</p>
                     <p className="body">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
-                <p className="subtitle"> Thank you</p>
-                    <p className="body">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
-                <p className="subtitle"> Your Data</p>
-                <p className="body">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
-                <a href="https://github.com/Tempoture">
-                    <img src={gitLogo} 
-                    width="67px" height="67px"/>
-                </a>
-                <img src={image} width="75px" height="65px"/>
-                <a href="https://rcos.io/">
-                    <img src={RcosR} 
-                    width="61px" height="61px"/>
-                </a>
-                <p className="footer">An RCOS Project :)</p>
+                    
+                    <div class="infoImg">
+                        <a href="https://github.com/Tempoture">
+                            <img src={gitLogo} 
+                            width="67px" height="67px"/>
+                        </a>
+                        <img src={image} width="75px" height="65px"/>
+                        <a href="https://rcos.io/">
+                            <img src={RcosR} 
+                            width="61px" height="61px"/>
+                        </a>
+                    </div>
+                    
+                    <p className="footer">An RCOS Project :)</p>
+                </div>
             </div>
        </content>
         );
