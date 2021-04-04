e48d7460605af1c503f3c0fed1ad9bee243e0138
Jimmers2001 <jdbatista2001@gmail.com>
2021-04-04 15:28:46 -0400

    Info Page's logo changes
    Changed the tempoture in the banner and the footer to the most updated images.

--- src/components/Pages/CurrentPages/InfoPage.js
+++ src/components/Pages/CurrentPages/InfoPage.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import image from '../../Navbar/Tempoture Logo.jpg'
+import TempLogo from '../CurrentPages/Tempo-Logo-Grey.png'
 import gitLogo from '../Images/GithubDesktopLogo.png'
 import RcosR from '../Images/RCOSlogo.png'
+import TempText from '../CurrentPages/Tempoture-Text.png'
 
 const InfoPage = () => {
     return (
         <content>
             <div class = "content">
                 <div class = "newcontainer">
-                    
-                    <p className="banner">Tempoture</p>
-                    <p className="subtitle">Information</p>
-                        <p className="body">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+                    <p className="banner">
+                    <img src={TempText} 
+                            width="400px"/>
+                    </p>
                     <p className="subtitle"> Thank you</p>
                         <p className="body">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
                     <p className="subtitle"> Your Data</p>
@@ -22,7 +23,7 @@ const InfoPage = () => {
                             <img src={gitLogo} 
                             width="67px" height="67px"/>
                         </a>
-                        <img src={image} width="75px" height="65px"/>
+                        <img src={TempLogo} width="75px" height="65px"/>
                         <a href="https://rcos.io/">
                             <img src={RcosR} 
                             width="61px" height="61px"/>
