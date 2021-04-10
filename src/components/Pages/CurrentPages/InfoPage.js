5e8e068e0b1a791812a6e0df859b165ff7e9e12e
Jimmers2001 <jdbatista2001@gmail.com>
2021-04-10 18:59:23 -0400

    Standardizing the content div

--- src/components/Pages/CurrentPages/InfoPage.js
+++ src/components/Pages/CurrentPages/InfoPage.js
@@ -12,39 +12,35 @@ const handleLogOut = () => {
 
 const InfoPage = () => {
     return (
-        <content>
-            <div class = "content_info">
-                <div class = "newcontainer">
-                    <p className="banner">
-                    <img src={TempText} 
-                            width="400px"/>
-                    </p>
-                    <p className="subtitle"> Thank you</p>
-                        <p className="infoBody">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
-                    <p className="subtitle"> Your Data</p>
-                    <p className="infoBody">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+        <div class = "newcontainer">
+            <p className="banner">
+            <img src={TempText} 
+                    width="400px"/>
+            </p>
+            <p className="subtitle"> Thank you</p>
+                <p className="infoBody">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+            <p className="subtitle"> Your Data</p>
+            <p className="infoBody">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
 
-                    <p className="footer">
-                        <div class="infoImg">
-                            <a href="https://github.com/Tempoture">
-                                <img src={gitLogo} 
-                                width="67px" height="67px"/>
-                            </a>
-                            <img src={TempLogo} width="75px" height="65px"/>
-                            <a href="https://rcos.io/">
-                                <img src={RcosR} 
-                                width="61px" height="61px"/>
-                            </a>  
-                        </div>
-                        An RCOS Project :)
-                    </p>
-                    <Button variant="info" type="submit" onClick={handleLogOut}>
-                                Logout {process.env.SPOTIFY_CLIENT_ID}
-                    </Button> 
+            <p className="footer">
+                <div class="infoImg">
+                    <a href="https://github.com/Tempoture">
+                        <img src={gitLogo} 
+                        width="67px" height="67px"/>
+                    </a>
+                    <img src={TempLogo} width="75px" height="65px"/>
+                    <a href="https://rcos.io/">
+                        <img src={RcosR} 
+                        width="61px" height="61px"/>
+                    </a>
                 </div>
-            </div>
-       </content>
-        );
+                An RCOS Project :)
+            </p>
+            <Button variant="info" type="submit" onClick={handleLogOut}>
+                        Logout {process.env.SPOTIFY_CLIENT_ID}
+            </Button> 
+        </div>
+    );
 };
 
 export default InfoPage;
