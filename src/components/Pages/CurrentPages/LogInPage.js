14f1cb387cd48aa6df3ea15d99c294afd29141c1
crowlc3 <crowlc3@rpi.edu>
2021-04-25 23:11:49 -0400

    Scaling updates for login page

--- src/components/Pages/CurrentPages/LogInPage.js
+++ src/components/Pages/CurrentPages/LogInPage.js
@@ -41,35 +41,38 @@ const LogInPage = (props) => {
   return (
     <div className="login">
       <div class="container">
-        <div className = "logo">
-            <img src={Logo} width="150px"></img>
+        <div className="login_text">
+          <div className = "logo">
+              <img src={Logo} width="150px"></img>
+          </div>
+          <div className = "text">
+              <img src={Text_} width="250px"></img>
+          </div>
+          <div className = "RCOS">
+            An <a className = "RcosLink" href = "https://rcos.io/">RCOS</a> Project
+          </div>
+          <div className = "Version">
+              Version 1.0 Coming Summer 2021
+          </div>
+          <a className = "What" href = "/LearnMore">
+              What is Tempoture?
+          </a>
+          <div className = "Agree">
+            By checking the user agreement below I hereby expressively consent for this app to be able to use website tracking in regard to location data and to communicate with my Spotify account's currently playing data, recently played data, playback state data and playlist data for the next 30 days.
+          </div>
+          <div className="Checkbox">
+            <input type="checkbox" className="Check" id="check" />
+            <label className="Label"> Accept User Agreement</label>
+          </div>
         </div>
-        <div className = "text">
-            <img src={Text_} width="250px"></img>
         </div>
-        <div className = "RCOS">
-          An <a className = "RcosLink" href = "https://rcos.io/">RCOS</a> Project
+          <Button variant="info" type="submit" onClick={handleLogin}>
+            Login to Spotify {process.env.SPOTIFY_CLIENT_ID}
+          </Button>
+        
+        <div className = "Spotify">
+          Don't have Spotify? <a className = "Click" href = "https://www.spotify.com/us/">Click here</a>
         </div>
-        <div className = "Version">
-            Version 1.0 Coming Summer 2021
-        </div>
-        <a className = "What" href = "/LearnMore">
-            What is Tempoture?
-        </a>
-        <div className = "Agree">
-          By checking the user agreement below I hereby expressively consent for this app to be able to use website tracking in regard to location data and to communicate with my Spotify account's currently playing data, recently played data, playback state data and playlist data for the next 30 days.
-        </div>
-        <div className="Checkbox">
-          <input type="checkbox" className="Check" id="check" />
-          <label className="Label"> Accept User Agreement</label>
-        </div>
-        <Button variant="info" type="submit" onClick={handleLogin}>
-          Login to Spotify {process.env.SPOTIFY_CLIENT_ID}
-        </Button>
-      </div>
-      <div className = "Spotify">
-        Don't have Spotify? <a className = "Click" href = "https://www.spotify.com/us/">Click here</a>
-      </div>
     </div>
   );
 };
